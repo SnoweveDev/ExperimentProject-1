63973aec2a623a6634a5df037f64652ebb7e1ca1
Tuyuki <Tuyuki0722@gmail.com>
2023-11-09 23:05:48 +0800

    測試Action

--- ExperimentProject-1/Assets/Scripts/Test/Domain_Player.cs
+++ ExperimentProject-1/Assets/Scripts/Test/Domain_Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class Domain_Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
